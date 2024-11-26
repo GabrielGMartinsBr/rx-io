aad2a6bd7d4e812c8598c67da0fe8b8d278fe126
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2024-11-26 06:13:08 -0300

    feat: Setup mruby lib

---
+++ cmake/FindMRuby.cmake
@@ -0,0 +1,32 @@
+# Locate the ruby-static library and include directories
+
+set(MRubyLibDir "./3rd/mruby")
+
+# Find the MRuby library and headers
+# This module defines:
+#   MRUBY_FOUND        - set to TRUE if MRuby is found
+#   MRUBY_INCLUDE_DIR  - include directory for MRuby
+#   MRUBY_LIBRARIES    - libraries to link against MRuby
+#   MRUBY_EXECUTABLES  - MRuby-related binaries (optional)
+
+find_path(MRUBY_INCLUDE_DIR
+    NAMES mruby.h
+    PATHS
+        ${MRubyLibDir}/include
+        ENV PATH
+    NO_DEFAULT_PATH
+)
+
+find_library(MRUBY_LIBRARIES
+    NAMES mruby mruby_core
+    PATHS
+        ${MRubyLibDir}/lib
+        ENV PATH
+    NO_DEFAULT_PATH
+)
+
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(MRuby REQUIRED_VARS MRUBY_INCLUDE_DIR MRUBY_LIBRARIES)
+
+mark_as_advanced(MRUBY_INCLUDE_DIR MRUBY_LIBRARIES)
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
+#include <mruby.h>
+#include <mruby/compile.h>
+#include <stdio.h>
+
 #include <iostream>
 
 int main()
 {
-  std::cout << "I works!" << std::endl;
+  std::cout << "It works!" << std::endl;
+
+  mrb_state *mrb = mrb_open();
+  if (!mrb) { /* handle error */
+  }
+  puts("Executing Ruby code from C!");
+  mrb_load_string(mrb, "p 'hello world!'");
+  mrb_close(mrb);
+  return 0;
 
   return 0;
 }
